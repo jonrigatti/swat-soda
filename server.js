c1936e8f0b0e802df7340650d36a6b6831bc4d8f
jonrigatti <jon.rigatti@live.com>
2025-03-31 13:46:19 -0400

    Deleted corsOptions

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var corsOptions = {
     origin: 'http://localhost:3000'
 };
 
-app.use(cors(corsOptions));
+app.use(cors());
 
 // parse requests of content-type - applicaton/json
 app.use(bodyParser.json());
